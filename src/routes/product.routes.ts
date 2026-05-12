7e4aea16c0dd4bfb52e0408f43553878e7e2f676
Sergio <201801656@est.umss.edu>
2026-05-12 14:45:53 -0400

    Avance matriz ventas

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { createProduct , getKardex, getProducts, updateProduct } from '../controllers/product.controller'
+import { createProduct , getKardex, getKardexPro, getProducts, updateProduct } from '../controllers/product.controller'
 
 const router = Router()
 
@@ -7,5 +7,6 @@ router.post('/create-product', createProduct)
 router.get('/get-products', getProducts)
 router.put('/update-product/:id', updateProduct)
 router.post('/kardex', getKardex)
+router.post('/kardex-pro', getKardexPro)
 
 export default router
